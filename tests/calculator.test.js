a5c464e80014f96add6fadfaceaf1cb281c3c21b
Daniela-2605 <daniela.gomez02@unicatolica.edu.co>
2025-10-19 23:24:33 -0500

    Subiendo el porcentaje de cobertura

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -3,3 +3,7 @@ const { add } = require('../src/calculator');
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
 });
+
+test('returns 0 when a is negative', () => {
+  expect(add(-5, 3)).toBe(0);
+});
